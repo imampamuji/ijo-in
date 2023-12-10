f7223837731390c2a69e32348b30ae3002830823
Imam Pamuji <imampamuji.2021@student.uny.ac.id>
2023-12-10 13:38:55 +0700

    Revert "ready presentation"
    This reverts commit 2be194d9fb475c6b8bfdf1f34c2818ed51dfc353.

--- app/src/main/java/com/example/ijo_in/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/ijo_in/ui/login/LoginFragment.kt
@@ -15,6 +15,7 @@ import org.koin.android.ext.android.inject
 
 class LoginFragment : Fragment() {
 
+
     private  var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
     private val viewModel: LoginViewModel by inject()
@@ -32,6 +33,11 @@ class LoginFragment : Fragment() {
 
     private fun observeData() {
         with(viewModel) {
+//            observeIsLogin().observe(requireActivity()) { data ->
+//                if (data != null) {
+//                    findNavController().navigate(R.id.action_loginFragment_to_navigationParentFragment)
+//                }
+//            }
             observeIsLogin().observe(requireActivity()) {
                 it.let { data ->
                     if (data != null) {
